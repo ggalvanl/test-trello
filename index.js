e8dff5094a2c032f64e4ab74c7bd0034eb7d2c63
Gerardo Galván <ggalvanl@xcaret.com>
2023-01-31 14:52:39 -0600

    mis cambios en main 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+console.log("Hello World con cambio en main");
 const varUno = "cambio 1";
 const varDos = "cambio 2 con cambio en main";
 const varTres = "cambio en main";
